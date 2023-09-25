0b227c7f39b97f4260c1f05a680d772e2a3bae09
Reshma <reshmasekhar27@gmail.com>
2023-09-25 01:54:02 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿
+using Exam53.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Exam53.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User stud)
+        {
+            return View(stud);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Kids()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Adult()
+        {
+            return View();
+
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Exam53.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Hobbies { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
